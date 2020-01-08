b8ba528e91b9482b15369c9bb5cdc80f5c43d1a5
varan <varan2030@mail.ru>
2020-01-07 20:15:49 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	s3: {
+		REGION: "us-east-1",
+		BUCKET: "note-app-course"
+	},
+	apiGateway: {
+		REGION: "us-east-1",
+		URL: "https://x21ovtgqi2.execute-api.us-east-1.amazonaws.com/prod"
+	},
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_fuxUUcxoX",
+		APP_CLIENT_ID: "30dsej7ndqonemn0k89e68pk9",
+		IDENTITY_POOL_ID: "us-east-1:a2eeeba0-6991-48a3-acd8-738b5d4d7ca2"
+	}
+};
