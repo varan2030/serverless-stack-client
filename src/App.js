59b53d55e7b7175b48c5abbf58fdc0f4539469e6
varan <varan2030@mail.ru>
2020-01-08 19:28:46 -0500

    Added Logout button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
@@ -6,6 +6,12 @@ import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 
 function App(props) {
+	const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+	function handleLogout() {
+		userHasAuthenticated(false);
+	}
+
 	return (
 		<div className="App container">
 			<Navbar fluid collapseOnSelect>
@@ -17,16 +23,22 @@ function App(props) {
 				</Navbar.Header>
 				<Navbar.Collapse>
 					<Nav pullRight>
-						<LinkContainer to="/signup">
-							<NavItem>Signup</NavItem>
-						</LinkContainer>
-						<LinkContainer to="/login">
-							<NavItem>Login</NavItem>
-						</LinkContainer>
+						{isAuthenticated ? (
+							<NavItem onClick={handleLogout}>Logout</NavItem>
+						) : (
+							<>
+								<LinkContainer to="/signup">
+									<NavItem>Signup</NavItem>
+								</LinkContainer>
+								<LinkContainer to="/login">
+									<NavItem>Login</NavItem>
+								</LinkContainer>
+							</>
+						)}
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
-			<Routes />
+			<Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
 		</div>
 	);
 }
