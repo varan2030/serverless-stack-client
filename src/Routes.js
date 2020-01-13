9cf5af6de04ebbd9c066f7e4a2c56d02d0532498
varan <varan2030@mail.ru>
2020-01-12 20:41:26 -0500

    Modifided Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
-import AppliedRoute from "./components/AppliedRoute";
 import ResetPassword from "./containers/ResetPassword";
 import Settings from "./containers/Settings";
 import ChangePassword from "./containers/ChangePassword";
@@ -19,12 +18,7 @@ import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 export default function Routes({ appProps }) {
 	return (
 		<Switch>
-			<UnauthenticatedRoute
-				path="/"
-				exact
-				component={Home}
-				appProps={appProps}
-			/>
+			<AuthenticatedRoute path="/" exact component={Home} appProps={appProps} />
 			<UnauthenticatedRoute
 				path="/login"
 				exact
